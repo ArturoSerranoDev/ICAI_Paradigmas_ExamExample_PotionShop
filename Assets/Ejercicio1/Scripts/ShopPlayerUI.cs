6c30247aee0fe437ca3ebf32e915e9dd89258201
ArturoSerranoDev <asm.arturo.developer@gmail.com>
2023-12-05 13:12:56 +0100

    Added ShopPlayerUI.cs

---
+++ Assets/Ejercicio1/Scripts/ShopPlayerUI.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShopPlayerUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI healthText;
+    
+    // healthText.text = "Health: 50";
+}
